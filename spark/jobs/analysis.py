bd35a152c6ed80689be30bb31ca799a5e55bc59c
SuperMelon <3117324424@qq.com>
2026-06-17 14:03:24 +0800

    纠正

--- spark/jobs/analysis.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-spark = SparkSession.builder.appName("DoubanAnalysisPlaceholder").getOrCreate()
-
-print("PySpark image is ready. Use this file for A-1 and A-2 analysis later.")
-
-spark.stop()
